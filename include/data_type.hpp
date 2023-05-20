d9f822692b9d9ff00011b375e95e7dfe079cfc03
Metaeye <98631718+Metaeye@users.noreply.github.com>
2023-05-20 12:39:15 +0800

    Add files via upload

---
+++ include/data_type.hpp
@@ -0,0 +1,18 @@
+#ifndef _DATA_TYPE_H_
+#define _DATA_TYPE_H_
+
+#include <msp430g2553.h>
+
+typedef char i8;
+typedef unsigned char u8;
+
+typedef int i16;
+typedef unsigned int u16;
+
+typedef long i32;
+typedef unsigned long u32;
+
+typedef float f32;
+typedef double f64;
+
+#endif // !_DATA_TYPE_H_
